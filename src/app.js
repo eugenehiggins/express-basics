85ac8948eaec6349b15efc7ca0e70d7c6df28473
eugenehiggins <genotype@gknow.com>
2015-09-17 11:45:35 -0600

    saving course changes

--- src/app.js
+++ src/app.js
@@ -11,10 +11,22 @@ app.get('/', function(req, res){
 	res.send("<h1>I am loving Treehouse!</h1>");
 });
 
-app.get('/blog', function(req, res){ 
-	res.send(posts);
+app.get('/blog/:title?', function(req, res){ 
+	debugger;
+	var title = req.params.title;
+
+	if (title === undefined) {
+		res.status(503);
+		res.send("This page is under construction!");
+
+	} else {
+
+		var post = posts[title];
+		res.send(post);
+	}
 });
 
 app.listen(3000, function(){
 	console.log("The frontend server is running on port 3000!")
 });
+
